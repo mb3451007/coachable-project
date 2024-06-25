455f96c70702aa258c515e4dcb295e4c1aa005e5
mb3451007 <mb3451007@gmail.com>
2024-06-25 15:08:05 +0500

    back end url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://coachable-backend-env.eba-mtutg33x.eu-central-1.elasticbeanstalk.com/api',
+    baseUrl: 'http://ec2-35-158-211-56.eu-central-1.compute.amazonaws.com/api',
     coachUrl: '/coach/',
     courseUrl: '/course/'  
   };
